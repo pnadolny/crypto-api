e9e4745c6f4bf61b30702fa4364819459bb16f66
paulnadolny <Paul.Stephen.Nadolny@ibm.com>
2021-11-14 18:40:37 -0600

    more columns

--- quotes.py
+++ quotes.py
@@ -56,8 +56,10 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   response_data = json.loads(response.text)
+  print()
+  print("Timestamp:"+response_data.get("status").get("timestamp"))
   pt = PrettyTable()
-  pt.field_names = ["Coin", "Name","Price", "1h","24h", "7d","Cost", "Balance", "G/L","%"]
+  pt.field_names = ["Coin", "Name","Price", "1h","24h", "7d","Vol24h","Cost", "Balance", "G/L","%"]
   total_cost, total_balance = 0,0
   for _, (symbol, coin) in enumerate(response_data.get("data").items()):
         quote = coin.get("quote").get("USD")
@@ -68,14 +70,28 @@ try:
         gain_loss = round(balance - cost,2)
         gain_loss_no_fees = 0
         perc = ((balance - cost) /  cost) * 100
-        pt.add_row([B +symbol+ N, coin.get("name"), formatCurrency(round(quote.get("price"),2)), colorMe(quote["percent_change_1h"]), colorMe(quote["percent_change_24h"]), colorMe(quote["percent_change_7d"]), formatCurrency(round(cost,2)), formatCurrency(round(balance,2)), colorMe(gain_loss), colorMe(perc)])
+        pt.add_row([B +symbol+ N, coin.get("name"),
+        formatCurrency(round(quote.get("price"),2)),
+        colorMe(quote["percent_change_1h"]),
+        colorMe(quote["percent_change_24h"]),
+        colorMe(quote["percent_change_7d"]),
+        colorMe(quote["volume_change_24h"]),
+        formatCurrency(round(cost,2)),
+        formatCurrency(round(balance,2)),
+        colorMe(gain_loss),
+        colorMe(perc)])
   pt.align = "r"
-  print(pt.get_string(sortby="G/L"))
+
   totalTable = PrettyTable()
   totalTable.field_names = ["Balance","Cost","G/L","%"]
-  gain_loss_percentage = round((total_balance - total_cost) / total_cost,2)
+  gain_loss_percentage = round((total_balance - total_cost) / total_cost,3)
+  totalTable.align = "r"
   totalTable.add_row([formatCurrency(round(total_balance,2)),formatCurrency(round(total_cost,2)),colorMe(total_balance-total_cost),colorMe(gain_loss_percentage)])
   print(totalTable)
+
+  print(pt.get_string(sortby="G/L"))
+
+
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
